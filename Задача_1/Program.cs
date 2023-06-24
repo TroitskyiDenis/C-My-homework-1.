bbb140cfc8bc7b01a0d4bb48b8359631de6ae39d
unknown <dtroitskyi@gmail.comgit config --global user.name 'Denis>
2023-06-24 18:27:46 +0300

    Сделано

---
+++ Задача_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число максимальное.
+Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+Console.WriteLine($"{num1}");
+if (num1 < num2)
+Console.WriteLine($"Максимальное число {num2}");
